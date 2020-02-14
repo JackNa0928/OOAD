e7a34c53c37c1602c93428348beb5c08116a21bd
JackNa0928 <60862957+JackNa0928@users.noreply.github.com>
2020-02-14 14:56:42 +0800

    Update GUI.java
    Co-Authored-By: jieklee <jieklee@users.noreply.github.com>
    Co-Authored-By: Dharsh05 <dharsh05@users.noreply.github.com>
    Co-Authored-By: syakirah08 <syakirah08@users.noreply.github.com>

--- src/OOAD/GUI.java
+++ src/OOAD/GUI.java
@@ -7,22 +7,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 
-<<<<<<< HEAD
-import javax.swing.*;
-import java.awt.*;
-import java.util.*;
-import java.awt.event.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-
 public class GUI {
 
   JButton[][] btn;
 
   JFrame frame = new JFrame("ANT & BUGS");
+    private String Counter;
 
-  public GUI(){
+    public GUI(){
 
     //panel board
 
@@ -62,51 +54,4 @@ public class GUI {
     new GUI();
   }
 }
-=======
 
-public class testgame  {
-
-    JButton[][] btn;
-
-    JFrame frame = new JFrame("ANT & BUGS");
-
-    public testgame(){
-
-        //panel board
-
-        TestBoard();
-
-    }
-
-    public void TestBoard(){
-        JPanel gameBoard = new JPanel(new GridLayout(20,20));
-        btn= new JButton[20][20];
-        for(int y=0;y<20;y++){
-            for(int x=0;x<7;x++){
-                btn[x][y]=new JButton();
-                btn[x][y].setBackground(Color.blue);
-                frame.add(btn[x][y]);
-                gameBoard.add(btn[x][y]);
-                btn[x][y].addActionListener(new MyActionListener());
-            }
-        }
-        //frame.add(topBoard,BorderLayout.NORTH);
-        frame.add(gameBoard,BorderLayout.CENTER);
-        frame.setSize(600,600);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
-    public static void main(String[] args){
-        new testgame();
-    }
-
-    private class MyActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    }
-}
->>>>>>> 98bb2847ab7ed1c817d61e27293a28d0b7b9ddeb
